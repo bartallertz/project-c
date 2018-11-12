28d97d156dd1f1902dde7f46ed037478090e292f
Rajiv_Laptop <0917711@hr.nl>
2018-11-12 14:55:34 +0100

    general update

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using projectC.model;
+
+namespace projectC.Controllers
+{
+    [Route("api/[controller]")]
+    public class CategoriesController : Controller
+    {
+
+        ProjectContext _context;
+
+        public CategoriesController(ProjectContext context)
+        {
+            this._context = context;
+        }
+        // GET api/values
+        [HttpGet]
+        public IQueryable<Category> Get()
+        {
+            var result = from m in this._context.categories select m;
+
+            return result;
+        }
+    }
+}
