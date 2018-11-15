ae6a245bb7f3a25831d981b3ae9fa202e7071edc
SaleemTheKing <37180925+SaleemTheKing@users.noreply.github.com>
2018-11-15 12:48:15 +0100

    fav fix

--- Controllers/FavouritesController.cs
+++ Controllers/FavouritesController.cs
@@ -55,7 +55,7 @@ namespace projectC.Controllers
 
         // POST api/values
         [HttpPost]
-        public IActionResult Post([FromBody]Favourite f)
+        public IActionResult Post([FromBody]Favorite f)
         {
             if (f == null)
             {
